1ad6b4b2f8ac1670483ebedde19fc8685b05e66b
woong <inwoong100@gmail.com>
2023-04-18 20:10:10 +0900

    chore: set tailwindcss userColor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
+    colors: { main: '#E11299', subOne: '#FFEAEA', subTwo: '#F5C6EC' },
+    fontFamily: {},
     extend: {},
   },
   plugins: [],
