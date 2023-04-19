7ee4e6ce2488df699a7872c2da105cf69cc88315
woong <inwoong100@gmail.com>
2023-04-19 13:32:09 +0900

    feat: modal page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FundPage from './pages/FundPage';
 import FundSelectPage from './pages/FundSelectPage';
 import MainPage from './pages/MainPage';
+import SplashPage from './pages/SplashPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/loading' element={<SplashPage />} />
         <Route path='/' element={<MainPage />} />
         <Route path='/fund' element={<FundPage />} />
         <Route path='/fund/:id' element={<FundSelectPage />} />
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,7 @@
-import { Fragment, useRef, useState } from 'react';
+import { Fragment, useRef } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
 import { Link } from 'react-router-dom';
 
 export default function Modal({ open, setOpen }) {
@@ -26,8 +27,8 @@ export default function Modal({ open, setOpen }) {
           <div className='fixed inset-0 transition-opacity' />
         </Transition.Child>
 
-        <div className='fixed  inset-0 bg-gray-light opacity-45 z-10 overflow-y-auto'>
-          <div className='flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0'>
+        <div className='fixed inset-0 overflow-y-auto bg-gray bg-opacity-70'>
+          <div className='flex min-h-full items-center justify-center p-4 text-center'>
             <Transition.Child
               as={Fragment}
               enter='ease-out duration-300'
@@ -37,38 +38,29 @@ export default function Modal({ open, setOpen }) {
               leaveFrom='opacity-100 translate-y-0 sm:scale-100'
               leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
-              <Dialog.Panel className='relative transform bg-subTwo opacity-100 overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+              <Dialog.Panel className='relative transform bg-white opacity-100 overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
                 <div className='bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
-                  <div className='sm:flex sm:items-start'>
-                    <div className='mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10'>
-                      <ExclamationTriangleIcon
-                        className='h-6 w-6 text-red-600'
-                        aria-hidden='true'
-                      />
-                    </div>
-                    <div className='mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left'>
-                      <Dialog.Title
-                        as='h3'
-                        className='text-base font-semibold leading-6 text-gray-900'
-                      >
-                        제목 입력
-                      </Dialog.Title>
-                      <div className='mt-2'>내용입력</div>
-                    </div>
-                  </div>
-                </div>
-                <div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'>
+                  <img src='/img/poster.png' alt='poster' />
                   <Link
                     to='/fund'
+                    className=' bg-white flex justify-center items-center absolute top-[290px] left-[40px] bg-transparent hover:bg-subTwo text-blue-700  hover:text-white py-1 px-14  rounded-xl'
+                  >
+                    <span className='text-xs text-font'>신청하러가기</span>
+                  </Link>
+                </div>
+                <div className='bg-gray-50 space-x-2 px-2 py-3 flex '>
+                  <button
+                    to='/fund'
                     type='button'
-                    className='inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto'
+                    className='inline-flex flex-4 justify-around items-center rounded-md px-3 py-2 text-sm font-semibold shadow-md hover:bg-subTwo'
                     onClick={() => setOpen(false)}
                   >
-                    이동하기
-                  </Link>
+                    <FontAwesomeIcon icon={faSquare} />
+                    &nbsp; 오늘 하루 보지 않기
+                  </button>
                   <button
                     type='button'
-                    className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
+                    className='inline-flex flex-1 justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
                     onClick={() => setOpen(false)}
                     ref={cancelButtonRef}
                   >
